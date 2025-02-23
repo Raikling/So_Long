0926367e2f85ba69ff8dd54aec71d852e2684633
raiklings <raiklings@outlook.com>
2025-02-23 18:35:45 +0000

    save point

---
+++ so_long.h
@@ -0,0 +1,39 @@
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <X11/X.h>
+#include "minilibx-linux/mlx.h"
+#include "GNL/get_next_line.h"
+#include "ft_Printf/ft_printf.h"
+#include <fcntl.h>
+#include <math.h>
+
+
+#define IMG_PXL 50
+#define UP 119
+#define DOWN 115
+#define LEFT 97
+#define RIGHT 100
+#define ESC 65307
+
+typedef struct s_player
+{
+    int x;
+    int y;
+}   t_player;
+
+typedef struct 
+{
+    
+};
+
+
+
+
+
+
+#endif
