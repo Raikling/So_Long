ba8159397e2108ffab95e5f9148755cd10f7daed
raiklings <raiklings@outlook.com>
2025-03-08 18:18:06 +0000

    sp

--- map_printer.c
+++ map_printer.c
@@ -34,9 +34,9 @@ void print_movements(t_map *map)
     char *move;
 
     move = ft_itoa(map->moves);
-    ft_printf("\r\x1b[33;01m"); // Start color
+    ft_printf("\x1b[33;01m"); // Start color
     ft_printf("%s", move);      // Print number
     ft_printf("\x1b[0m");       // Reset color
-    ft_printf(" movements");    // Print "movements"
+    ft_printf(" movements\n");    // Print "movements"
     free(move);
 }
--- movements.c
+++ movements.c
@@ -1,11 +1,8 @@
  #include "so_long.h"
 
-
-
-
 static void move_resume(t_map *map, int *x, int *y, int direction)
 {
-    // Update position based on direction
+
     if (direction == UP)
         *x -= 1;
     if (direction == DOWN)
@@ -14,16 +11,11 @@ static void move_resume(t_map *map, int *x, int *y, int direction)
         *y -= 1;
     if (direction == RIGHT)
         *y += 1;
-
-    // Bounds checking to prevent out-of-bounds access
     if (*x < 0 || *x >= map->x || *y < 0 || *y >= map->y)
         return;
 
-    // Handle exit: return if player reaches 'E' and all collectibles are gathered
     if (map->array[*x][*y] == 'E' && map->c == 0)
         return (ft_win(map));
-
-    // Handle collectible: remove 'C' and decrement counter
     if (map->array[*x][*y] == 'C')
     {
         map->array[*x][*y] = '0';
@@ -39,17 +31,13 @@ void move_up(t_map *map)
     if (x > 0 && map->array[x - 1][y] != '1') // Check bounds and wall
     {
         move_resume(map, &x, &y, UP);
-        // Prevent move if hitting exit with collectibles remaining or exit already triggered
         if (map->array[x][y] == 'E' && (map->c != 0 || map->exit == 1))
             return;
-
         map->moves++;
         // Clear current position
         mlx_put_image_to_window(map->mlx, map->wnd, map->img.floor, y * IMG_PXL, map->player.x * IMG_PXL);
         map->array[map->player.x][y] = '0';
-        // Update player position
         map->player.x = x;
-        // Draw new position
         mlx_put_image_to_window(map->mlx, map->wnd, map->img.floor, y * IMG_PXL, x * IMG_PXL);
         mlx_put_image_to_window(map->mlx, map->wnd, map->img.player, y * IMG_PXL, x * IMG_PXL);
         map->array[x][y] = 'P';
